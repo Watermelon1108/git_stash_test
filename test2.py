94ec002f77e48c78859fb814857cb5f838c81af0
huiminga <huimin.gao@intel.com>
2020-04-08 14:33:48 +0800

    update test2

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
 print("hello, this test2.py in master branch")
 print("add this content for modified laster")
 print("this line will be modified later")
+----this line will be removed------
+------this line will be modifed----
